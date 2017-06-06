66e4c118abae5f820188f41999be29770470b881
Anders Øen Fylling <sciencefyll@users.noreply.github.com>
2017-06-06 21:38:06 +0200

    Create definitions.h

---
+++ include/uci/definitions.h
@@ -0,0 +1,8 @@
+namespace uci {
+
+// Holds the keys and associated values per UCI input.
+typedef std::map<std::string, std::string> arguments_t;
+
+// The lambda type which is used to add a function listener to an event.
+typedef std::function<void(arguments_t)> callback_t;
+}
