472ae2b2d030cf270d960e9a75bd5b4715745154
Anders Øen Fylling <sciencefyll@users.noreply.github.com>
2017-06-06 21:39:13 +0200

    Create events.h

---
+++ include/uci/events.h
@@ -0,0 +1,24 @@
+namespace uci {
+
+// Different commands created by the UCI protocol
+namespace event {
+
+// errors, this isn't really a UCI protocol command, but used within this project.
+static const uint8_t NO_MATCHING_COMMAND = 0;
+
+// commands issued by the UCI protocol
+static const uint8_t TEST = 5;
+static const uint8_t UCI = 10;
+static const uint8_t DEBUG = 20;
+static const uint8_t ISREADY = 30;
+static const uint8_t SETOPTION = 40;
+static const uint8_t REGISTER = 50;
+static const uint8_t UCINEWGAME = 60;
+static const uint8_t POSITION = 70;
+static const uint8_t GO = 80;
+static const uint8_t STOP = 90;
+static const uint8_t PONDERHIT = 100;
+static const uint8_t QUIT = 110;
+
+}
+}
