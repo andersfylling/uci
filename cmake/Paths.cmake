6d8a4132c786e8b18978d05595d8e678746b3d00
Anders Øen Fylling <fyllingz@gmail.com>
2017-06-08 01:19:04 +0200

    fixed gitmodule behavior when used as a git-submodule

--- cmake/Paths.cmake
+++ cmake/Paths.cmake
@@ -1,5 +1,10 @@
-set(UCI_BENCHMARK_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/benchmark/include")
-set(UCI_CATCH_INCLUDE_DIR     "${CMAKE_CURRENT_SOURCE_DIR}/external/Catch/include")
+# External project files
+# These are ignored if this is used as a external / sub module
+if (${CMAKE_CURRENT_SOURCE_DIR} EQUAL ${CMAKE_SOURCE_DIR})
+    set(UCI_BENCHMARK_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/benchmark/include")
+    set(UCI_CATCH_INCLUDE_DIR     "${CMAKE_CURRENT_SOURCE_DIR}/external/Catch/include")
+endif()
 
+# Main project files
 set(UCI_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src/include")
 set(UCI_PROJECT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src")
